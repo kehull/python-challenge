71fffff59cd2ba98a3e1d635a353f7fb2cc20f94
kehull <71107280+kehull@users.noreply.github.com>
2020-11-15 22:05:52 -0600

    just need to create txt file
    In the process of working on this code, I learned that I had a very weak grasp on scope in loops. By basing my code off the general structure of code from Saturday's classwork, I managed to accidentally set up two loops, which prevented my file from ever functioning as expected. Once I was able to identify and fix that issue, the rest of the code fell into place fairly easily. Once I add the code that will cause this file to create a new text file featuring the requested summary table at the completion of the loop, I will be ready to turn this in for grading.

--- Pybank/main.py
+++ Pybank/main.py
@@ -4,36 +4,53 @@ import csv
 # Path to collect data from the Resources folder
 bank_csv = os.path.join( 'Resources', 'budget_data.csv')
 
-def summary(bank_info):
-    date = str(bank_info[0])
-    money = int(bank_info[1])
-
-
-    Total_Months = sum(1 for row in csvreader) #count rows of document
-    Total = "TBA" #total of money column
-    Avg_Change = "TBA" #Total / Total_Months
-    Greatest_Increase = "TBA" #this will be a formula!
-    Greatest_Decrease = "TBA" #this will be a formula!
-
     #Set up code to process final results
+#define the summary function, which will iterate through csvreader
+def summary(csvreader):
+    #establish variables with global scope outside of the loop
+    Total_Months = 0
+    Total = 0
+    Changes = []
+    Difference = []
+    Greatest_Increase = 0
+    Greatest_Decrease = 0
+    Avg_Change = 0
+    
+    #begin loop
+    for row in csvreader:
+        #give the columns of the csv variable names to make them easier to call on later
+        date = str(row[0])
+        money = int(row[1])
+        Total_Months += 1 #+1 to Total_Months per row
+        Total = Total + money # add value from money to Total per row
+        Changes.append(money) #put profits/losses into Changes list
+        if len(Changes)>=2: #make sure there are enough items in the Changes list to perform the math functions inside of this if statement
+            Difference.append(Changes[-1] - Changes[-2]) #calculate the difference between the current month's profit/loss and the previous month's; append that value to Difference list
+        if len(Difference)>=1: #make sure there are enough items in the Difference list to perform the math functions inside of this if statement
+            if Difference[-1] > Greatest_Increase and Difference[-1] > 0: #compare the current Difference value to the largest Difference value thus far to determine if the new value is greater. Lesser values can't possibly be the greatest value, so those are ignored. 
+                Greatest_Increase = Difference[-1]  #reset the previous value to the greater value
+                Greatest_Increase_Date = (row[0]) #store the date from the current row as a variable so it can be called later in a print statement
+            if abs(Difference[-1]) > abs(Greatest_Decrease) and Difference[-1] < 0: #compare the absolute value of the current Difference value to the largest Difference value thus far to determine if the new value is greater.
+                Greatest_Decrease = Difference[-1] #reset the previous value to the greater value
+                Greatest_Decrease_Date = (row[0]) #store the date from the current row as a variable so it can be called later in a print statement
+        if len(Difference)>=2: #make sure therea re enought items in the Difference list to perform the math functions inside of this if statement
+            Avg_Change = sum(Difference)/len(Difference) #perform calculation to determine the average change from month to month
+
+    #Set up code to process final results to terminal
     print(f"Financial Analysis")
     print(f"----------------------------")
     print(f"Total Months: {Total_Months}")
-    print(f"Total: {Total}")
-    print(f"Average_Change: {Avg_Change}")
-    print(f"Greatest Increase in Profits: {Greatest_Increase}")
-    print(f"Greatest Decrease in Profits: {Greatest_Decrease}")
+    print(f"Total: ${Total}")
+    print(f"Average Change: ${str(round(Avg_Change,2))}")
+    print(f"Greatest Increase in Profits: {Greatest_Increase_Date} (${Greatest_Increase})")
+    print(f"Greatest Decrease in Profits: {Greatest_Decrease_Date} (${Greatest_Decrease})")
 
-
-
-#Add a line of code that will export a text file with the results
-
+#still need to write the code to create a txt file containing all of the print statements in the previous block
 
 # Read in the CSV file
 with open(bank_csv, 'r') as csvfile:
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
-    for row in csvreader:
-        summary(row)
+    summary(csvreader)
    
