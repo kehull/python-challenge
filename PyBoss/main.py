ed7c282cb6d31980f755bf2497221799570653fa
kehull <71107280+kehull@users.noreply.github.com>
2020-11-21 00:19:46 -0600

    still working on PyBoss
    Most of the way there, just not sure what to do about the states yet.

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -11,39 +11,35 @@ DOB = []
 SSN = []
 State = []
 
-# Use encoding for Windows
-
+# open the file
 with open(employee_data, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader)
     for row in csvreader:
-        # Add Employee ID
+        # Add Employee ID to list
         Emp_ID.append(row[0])
-
-        # Split name and add to First Name and Last Name
-        str(row[1]).split(' ')
-        First_Name.append(row[1])
-        Last_Name.append(row[2])
-
-        # Add DOB
-        DOB.append(row[3])
-
-        # Add SSN
-        SSN.append(row[4])
+        # Split name and add to First Name and Last Name lists
+        Full_Name = str(row[1]).split(' ')
+        First_Name.append(Full_Name[0])
+        Last_Name.append(Full_Name[1])
+        # Format birthdate adn add to DOB
+        Split_DOB = str(row[2]).split('-')
+        DOB.append(f'{Split_DOB[1]}/{Split_DOB[2]}/{Split_DOB[0]}')
+        # Obscure first five digits of string and add to SSN
+        Split_SSN =str(row[3]).split('-')
+        SSN.append(f'***-**-{Split_SSN[2]}') 
+        # Add State
+        State.append(row[4]) #still working on this
 
 
 # Zip lists together
 cleaned_csv = zip(Emp_ID, First_Name, Last_Name, DOB, SSN, State)
-
 # Set variable for output file
 output_file = os.path.join("cleaned_data","pyboss_complete.csv")
-
 #  Open the output file
 with open(output_file, "w", newline="") as datafile:
     writer = csv.writer(datafile)
-
     # Write the header row
     writer.writerow(["Employee ID", "First Name", "Last Name", "DOB", "SSN", "State"])
-
     # Write in zipped rows
     writer.writerows(cleaned_csv)
-    print(writer)
