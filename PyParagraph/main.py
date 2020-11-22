46a29aca75146a302b64703b18b2ef1b9929eb0c
kehull <71107280+kehull@users.noreply.github.com>
2020-11-21 21:58:33 -0600

    PyParagraph complete
    This was fairly straightforward to write, and involved learning a bit about regular expressions. I actually have a lot of experience with word/sentence counters, as I used to participate in NaNoWriMo (an event in which people attempt to write a 50k word first draft of a novel) every year. A very very common user issue for the event is that people type up their novel in MSWord or Google Docs or any number of other programs, believe they've written 50k words, then submit their story to the NaNoWriMo word count verifier and find out they're several thousand words short of the goal. In my comments, I've included some thoughts on the word/sentence counting methods I've used.

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,17 +1,20 @@
 import os
 import csv
+import re
 
+#open the file and read it in
 paragraph_file = os.path.join("Mathematicians_Lament_Section.txt")
-
 with open(paragraph_file, "r") as text:
     lines = text.read()
-    print(lines)
 
-    Word_Count = 0
-    Sentence_Count = 0
-    Word_Length = 0
-    Sentence_Length = 0
+    #do the calculations
+    Word_Count = int(len(lines.split())) #This is not a perfect way to count words- for example, it counts hyphenated words and phrases as single words, which could be an issue if you want a phrase such as "well-to-do" to count as three words. However, in most cases it works fine as an approximation.
+    Sentence_Count = int(len(lines.split("(?<=[.!?]) +"))) #This is not a perfect way to count sentences- for example, it would count a sentence like "The planet closest to the sun (ie Mercury) has the most extreme temperature variations." as three separate sentences. However, in most cases it works fine as an approximation.
+    Sentence_Length = round((Word_Count/Sentence_Count),1)
+    All_Letters = int(len(re.sub("(?<=[.!?]) +", '', str(lines)))) #This code removes all characters listed in the first argument of re.sub() from the text and returns the remaining characters as items a list, then returns the length of that list.
+    Word_Length = round((All_Letters/Word_Count),1)
 
+#write summary table. The assignment did not specify do do this, so it may not be necessary, but I wanted more practice using this method.
 output_path = os.path.join("Paragraph_Analysis.txt") 
 with open (output_path,'w', newline='') as datafile:
         writer = csv.writer(datafile)
@@ -20,7 +23,7 @@ with open (output_path,'w', newline='') as datafile:
         writer.writerow([f'Approximate Word Count: {Word_Count}'])
         writer.writerow([f'Approximate Sentence Count: {Sentence_Count}'])
         writer.writerow([f'Average Letter Count: {Word_Length}'])
-        writer.writerow([f'Average Sentence Length:{Sentence_Length}'])
+        writer.writerow([f'Average Sentence Length: {Sentence_Length}'])
         print(writer)
 
 #open the Paragraph_Analysis.txt file and read it into the terminal
